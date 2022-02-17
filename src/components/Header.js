b5efa7d3337f76540b47d484bbb2f472e77d5bda
Sachin Chavan <sachincvn962@gmail.com>
2022-02-17 23:26:36 +0530

    responsive

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,22 +1,24 @@
 import React from "react";
 import Navbar from "./NavBar";
-import "./Header.css";
+import Footer from "./Footer";
+import './Header.css'
 
 const Header = () => {
   return (
     <>
-      <div className="header">
-      <Navbar />
+      
+        
+        <div className="header">
+        <Navbar/>
         <div className="content">
-          <p className="slide-left"> Welcome To Sharnbasva University </p>
-          <h1 className="slide-left">
-            Education is the best <br /> key success in life
-          </h1>
-          <div className="links slide-left">
-            <a  className="btn1" href="">Contact Us</a>
-          </div>
+            <p className="slide-left"> Welcome To sharnbasv auniversity</p>
+            <h1 className="slide-left">Education Is The <br/>Key To Success</h1>
+            <div className="links">
+            <a  className="btn1 slide-left" href="">Contact US</a>
+            </div>
         </div>
-      </div>
+        </div>
+		{/* <Footer/> */}
     </>
   );
 };
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,26 +2,42 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "./Navbar.css";
 
+
 const navBar = () => {
   return (
     <>
       <nav>
-        <div className="logo">
-          <h1>Sharnbasva University</h1>
-        </div>
-        <ul>
-          <li>
-          <Link to="/">Home</Link>
-          </li>
-          <li>
-          <Link to="/about">About Us</Link>
-          </li>
-          <li>
-            <a href="">Admission</a>
-          </li>
-          <li>
-            <a href="">Contact</a>
-          </li>
+        <div className="logo">The-SUK</div>
+        <ul className="nav-links">
+          <input type="checkbox" id="checkbox_toggle" />
+          <label for="checkbox_toggle" className="hamburger">
+            &#9776;
+          </label>
+          <div className="menu">
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li className="services">
+              <a href="/">Services</a>
+              <ul className="dropdown">
+                <li>
+                  <a href="/">Dropdown 1 </a>
+                </li>
+                <li>
+                  <a href="/">Dropdown 2</a>
+                </li>
+              </ul>
+            </li>
+            <li>
+              <a href="/">Pricing</a>
+            </li>
+            <li>
+              <a href="/">Contact</a>
+            </li>
+          </div>
         </ul>
       </nav>
     </>
